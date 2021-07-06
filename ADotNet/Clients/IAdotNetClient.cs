91e487da706f221d042954740c6e84769781b06e
Hassan Habib <harezk@microsoft.com>
2021-07-05 20:37:01 -0700

    CLIENTS: Add Interface

---
+++ ADotNet/Clients/IAdotNetClient.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace ADotNet.Clients
+{
+    public interface IAdotNetClient
+    {
+        void SerializeAndWriteToFile(object adoPipeline, string path);
+    }
+}
