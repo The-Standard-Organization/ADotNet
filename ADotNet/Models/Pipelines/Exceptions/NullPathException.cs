a4b18089c43b711fea81c3cc9e3b9425a0ed32e8
Hassan Habib <harezk@microsoft.com>
2021-07-05 19:54:57 -0700

    ShouldThrowValidationExceptionOnSerializeIfPathIsNull -> FAIL

---
+++ ADotNet/Models/Pipelines/Exceptions/NullPathException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace ADotNet.Models.Pipelines.Exceptions
+{
+    public class NullPathException : Exception
+    {
+        public NullPathException()
+            : base("Pipeline is null") { }
+    }
+}
--- AdoNet.Tests.Unit/Services/AdoServiceTests.Validations.cs
+++ AdoNet.Tests.Unit/Services/AdoServiceTests.Validations.cs
@@ -7,6 +7,7 @@
 using System;
 using ADotNet.Models.Pipelines.AspNets;
 using ADotNet.Models.Pipelines.Exceptions;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -32,8 +33,51 @@ namespace AdoNet.Tests.Unit.Services
                 this.adoService.SerializeAndWriteToFile(somePath, invalidPipeline);
 
             // then
-            Assert.Throws<AdoValidationException>(
-                serializeAndWriteToFileAction);
+            AdoValidationException actualAdoValidationException =
+                Assert.Throws<AdoValidationException>(
+                    serializeAndWriteToFileAction);
+
+            actualAdoValidationException.InnerException.Message.Should()
+                .BeEquivalentTo(nullPiplineException.Message);
+
+            this.yamlBrokerMock.Verify(broker =>
+                broker.SerializeToYaml(It.IsAny<object>()),
+                    Times.Never);
+
+            this.filesBrokerMock.Verify(broker =>
+                broker.WriteToFile(It.IsAny<string>(), It.IsAny<string>()),
+                    Times.Never);
+
+            this.yamlBrokerMock.VerifyNoOtherCalls();
+            this.filesBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldThrowValidationExceptionOnSerializeIfPathIsNull()
+        {
+            // given
+            string invalidPath = null;
+            AspNetPipeline someAspNetPipeline = CreateRandomAspNetPipeline();
+
+            var nullPathException =
+                new NullPathException();
+
+            var expectedAdoValidationException =
+                new AdoValidationException(nullPathException);
+
+            // when
+            Action serializeAndWriteToFileAction = () =>
+                this.adoService.SerializeAndWriteToFile(
+                    invalidPath, 
+                    someAspNetPipeline);
+
+            // then
+            AdoValidationException actualAdoValidationException = 
+                Assert.Throws<AdoValidationException>(
+                    serializeAndWriteToFileAction);
+
+            actualAdoValidationException.InnerException.Message.Should()
+                .BeEquivalentTo(nullPathException.Message);
 
             this.yamlBrokerMock.Verify(broker =>
                 broker.SerializeToYaml(It.IsAny<object>()),
