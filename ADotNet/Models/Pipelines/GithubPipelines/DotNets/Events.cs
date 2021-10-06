2902432e11faf48613a13561533fbdd537691635
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-05 22:11:51 -0700

    MODEL: Github model

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Events.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    public class Events
+    {
+        public PushEvent Push { get; set; }
+
+        [YamlMember(Alias = "pull_request")]
+        public PullRequestEvent PullRequest { get; set; }
+    }
+}
---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/GithubPipeline.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    public class GithubPipeline
+    {
+        public string Name { get; set; }
+
+        [YamlMember(Alias = "on")]
+        public Events OnEvents { get; set; }
+    }
+}
