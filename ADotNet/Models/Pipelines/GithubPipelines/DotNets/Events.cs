ad6165e96c1bd1092a6c9c6ba46f648d1f58c273
Hassan Habib <harezk@microsoft.com>
2021-10-06 21:14:38 -0700

    CODE RUB: Run Code Clean up & Copyrights

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/Events.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Events.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
 using YamlDotNet.Serialization;
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
