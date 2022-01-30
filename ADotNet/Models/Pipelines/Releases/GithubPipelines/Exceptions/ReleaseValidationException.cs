6d7c1ae545251b0af2d4e7f46f60f8c94819e767
Shri Humrudha <shrija@microsoft.com>
2022-01-30 14:01:19 -0800

    ShouldThrowValidationExceptionOnSerializeWriteIfPathIsNull -> FAIL

---
+++ ADotNet/Models/Pipelines/Releases/GithubPipelines/Exceptions/ReleaseValidationException.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ADotNet.Models.Pipelines.Releases.GithubPipelines.Exceptions
+{
+    public class ReleaseValidationException : Xeption
+    {
+        public ReleaseValidationException(Xeption innerException)
+            : base(message: "Release validation error occurred, fix the errors and try again.",
+                  innerException)
+        { }
+    }
+}
