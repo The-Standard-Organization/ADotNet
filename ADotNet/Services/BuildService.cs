ea0eeb44d3ed58eace72c396d1ec46d34f035cac
Maytham Fahmi <maythamfahmi@nextbix.dk>
2022-02-11 09:31:18 +0100

    Remove temp line after fix singl qoute issue

--- ADotNet/Services/BuildService.cs
+++ ADotNet/Services/BuildService.cs
@@ -30,11 +30,6 @@ namespace ADotNet.Services
             string serializedPipeline =
                 this.yamlBroker.SerializeToYaml(adoPipeline);
 
-            // todo: with out this line, the serialization will create single quote for Brackets and that breaks the yaml compilation.
-            // the question is there any better idea or way on how to generate Brackets with out single quote?
-            // working on a solution :)
-            serializedPipeline = serializedPipeline.Replace("'[", "[").Replace("]'", "]");
-
             this.filesBroker.WriteToFile(path, serializedPipeline);
         });
     }
