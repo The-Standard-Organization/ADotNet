a732d87db1ae5a05587ba6ce436f266bd8b713e8
Shri Humrudha <shrija@microsoft.com>
2022-02-12 11:32:08 -0800

    ShouldThrowValidationExceptionOnWriteIfPathIsInvalid -> FAIL

---
+++ ADotNet/Models/Services/Foundations/Files/Exceptions/FileValidationException.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace ADotNet.Models.Services.Foundations.Files.Exceptions
+{
+    public class FileValidationException : Xeption
+    {
+        public FileValidationException(Xeption innerException)
+            : base(message: "File validation error occurred, fix the errors and try again.",
+                  innerException)
+        { }
+    }
+}
