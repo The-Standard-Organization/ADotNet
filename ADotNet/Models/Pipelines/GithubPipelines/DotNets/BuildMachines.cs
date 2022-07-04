a08af625dfb8e38e168611d0f5013894ce20fb65
Hassan Habib <hassanhabib@live.com>
2022-07-04 10:49:40 -0700

    DATA: Add Windows Latest Option

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/BuildMachines.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/BuildMachines.cs
@@ -1,5 +1,5 @@
 ﻿// ---------------------------------------------------------------------------
-// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Copyright (c) Hassan Habib All rights reserved.
 // Licensed under the MIT License.
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------------------
@@ -11,5 +11,6 @@ namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
         public const string UbuntuLatest = "ubuntu-latest";
         public const string Windows2019 = "windows-2019";
         public const string Windows2022 = "windows-2022";
+        public const string WindowsLatest = "windows-latest";
     }
 }
