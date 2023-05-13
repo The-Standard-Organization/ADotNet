540c8652f3c69b5ac6f1d78001b59e32349994da
Greg Hays <glhays.dev@gmail.com>
2023-05-12 17:59:57 -0700

    DATA: Remove Hard Coded Needs

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/TagJob.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/TagJob.cs
@@ -13,12 +13,13 @@ namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
     public partial class TagJob
     {
         [YamlMember(Alias = "if")]
-        public string TagTrait = "github.event_name == 'pull_request' && github.event.action == 'closed'" +
-                " && github.event.pull_request.merged && startsWith(github.event.pull_request.title, 'RELEASES:')" + "" +
-                    " && contains(github.event.pull_request.labels.*.name, 'RELEASES')";
+        public string TagTrait = "github.event.pull_request.merged && github.event.pull_request.base.ref == 'main'" +
+                " && startsWith(github.event.pull_request.title, 'RELEASES:')" + 
+                    " && (contains(github.event.pull_request.labels.*.name, 'RELEASE')" + 
+                        " || contains(github.event.pull_request.labels.*.name, 'RELEASE'))";
 
         [YamlMember(Alias = "needs")]
-        public string[] Needs = { "build", "test" };
+        public string[] Needs { get; set; }
 
         [YamlMember(Alias = "runs-on")]
         public string RunsOn { get; set; }
