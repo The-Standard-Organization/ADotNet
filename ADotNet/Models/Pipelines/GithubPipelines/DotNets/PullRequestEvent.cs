9085631d6c2e86bd6cae3131e38013d019108b8b
Catalin-Andronie <catalin.andronie@gmail.com>
2023-05-15 00:47:22 +0300

    Allow configuring the types of pull-request event

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/PullRequestEvent.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/PullRequestEvent.cs
@@ -11,7 +11,10 @@ namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
 {
     public class PullRequestEvent
     {
-        [YamlMember(ScalarStyle = ScalarStyle.Folded)]
+        [YamlMember(Order = 0, ScalarStyle = ScalarStyle.Folded, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public string[] Types { get; set; }
+
+        [YamlMember(Order = 1, ScalarStyle = ScalarStyle.Folded)]
         public string[] Branches { get; set; }
     }
 }
