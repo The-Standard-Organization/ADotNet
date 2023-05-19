27340a6018cbd868c80230c58d97522f8116ec25
Christo du Toit <christo@dutoit.co.uk>
2023-05-19 12:01:14 +0100

    DATA: NuGet Publish Job

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/NuGetPublishJob.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    internal class NuGetPublishJob
+    {
+    }
+}
