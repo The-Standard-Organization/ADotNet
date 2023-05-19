a900bc38d2732837b0592b5d833a66d73dcd94ea
Christo du Toit <christo@dutoit.co.uk>
2023-05-19 11:30:15 +0100

    DATA: Addition of ReleaseTaskV1

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/ReleaseTaskV1.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
+{
+    public class ReleaseTaskV1 : GithubTask
+    {
+        [YamlMember(Order = 1, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public string Id { get; set; }
+
+        [YamlMember(Order = 2)]
+        public string Uses = "actions/create-release@v1";
+
+        [YamlMember(Order = 3, Alias = "env", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public Dictionary<string, string> EnvironmentVariables { get; set; }
+
+        [YamlMember(Order = 4, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public Dictionary<string, string> With { get; set; }
+    }
+}
