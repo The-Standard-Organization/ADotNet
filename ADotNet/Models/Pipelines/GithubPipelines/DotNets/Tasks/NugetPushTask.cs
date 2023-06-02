5abb2818f9e43248197144eb2d32def0bfacb451
Christo du Toit <christo@dutoit.co.uk>
2023-06-02 19:17:06 +0100

    INFRASTRUCTURE: Upgraded the NugetPushTask to work with the changes to the base class.

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/NugetPushTask.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/NugetPushTask.cs
@@ -8,9 +8,21 @@ using YamlDotNet.Serialization;
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
 {
-    public class NugetPushTask : GithubTask
+    /// <summary>
+    /// A task that allows you to push a package file to NuGet.
+    /// </summary>
+    public sealed class NugetPushTask : GithubTask
     {
-        [YamlMember(Order = 1)]
-        public string Run = "dotnet nuget push **/bin/Release/**/*.nupkg --source https://api.nuget.org/v3/index.json --api-key ${{ secrets.NUGET_ACCESS }} --skip-duplicate";
+        public NugetPushTask(string nugetApiKey)
+        {
+            Run = "dotnet nuget push **/bin/Release/**/*.nupkg --source "
+                + $"https://api.nuget.org/v3/index.json --api-key {nugetApiKey} --skip-duplicate";
+        }
+
+        /// <summary>
+        /// Gets the command to execute for the task.
+        /// </summary>
+        [YamlMember(Order = 7, Alias = "run")]
+        public new string Run { get; private set; }
     }
 }
