a8ba070da2ffedd3efc38c1cce5a776a989c2082
Christo du Toit <christo@dutoit.co.uk>
2023-06-02 19:22:58 +0100

    INFRASTRUCTURE: Created CreateGitHubTagTask

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/CreateGitHubTagTask.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
+{
+    /// <summary>
+    /// A task to build .NET project.
+    /// </summary>
+    public sealed class CreateGitHubTagTask : GithubTask
+    {
+        public CreateGitHubTagTask(string tagName, string tagMessage)
+        {
+            Run = $"git tag -a \"{tagName}\" -m \"{tagMessage}\"\r"
+                + "git push origin --tags";
+        }
+
+        /// <summary>
+        /// Gets the command to execute for the task.
+        /// </summary>
+        [YamlMember(Order = 7, Alias = "run")]
+        public new string Run { get; private set; }
+    }
+}
