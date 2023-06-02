181c3ec9c13d4fac57ee17474d73248bd82c0b4a
Christo du Toit <christo@dutoit.co.uk>
2023-06-02 19:33:12 +0100

    INFRASTRUCTURE: Created PublishJob

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/PublishJob.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/PublishJob.cs
@@ -5,27 +5,98 @@
 // ---------------------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.ComponentModel;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV3s;
 using YamlDotNet.Serialization;
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
 {
     public class PublishJob
     {
-        [YamlMember(Order = 0, Alias = "runs-on")]
-        public string RunsOn { get; set; }
+        public PublishJob(
+            string runsOn,
+            string dependsOn,
+            string nugetApiKey)
+        {
+            RunsOn = runsOn;
+            Needs = new string[] { dependsOn };
 
-        [YamlMember(Order = 1, Alias = "needs", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public string[] Needs { get; set; }
+            If = $"needs.{dependsOn}.result == 'success'";
 
-        [YamlMember(Order = 2, Alias = "if", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public string If { get; set; }
+            Steps = new List<GithubTask> {
+                new CheckoutTaskV3
+                {
+                    Name = "Check out"
+                },
 
-        [YamlMember(Order = 3, Alias = "env", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public Dictionary<string, string> EnvironmentVariables { get; set; }
+                new SetupDotNetTaskV3
+                {
+                    Name = "Setup .Net",
+
+                    With = new TargetDotNetVersionV3
+                    {
+                        DotNetVersion = "7.0.201"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restore"
+                },
+
+                new DotNetBuildReleaseTask
+                {
+                    Name = "Build",
+                },
+
+                new PackNugetTaskWithSymbols
+                {
+                    Name = "Pack NuGet Package",
+                },
+
+                new NugetPushTask(nugetApiKey)
+                {
+                    Name = "Push NuGet Package",
+                }
+            };
+
+        }
+
+        [YamlMember(Order = 0, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new string Name { get; private set; }
+
+        [YamlMember(Order = 1, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new string Environment { get; private set; }
+
+        [YamlMember(Order = 2, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new DefaultValues Defaults { get; private set; }
+
+        [YamlMember(Order = 3, Alias = "runs-on")]
+        public new string RunsOn { get; private set; }
 
         [YamlMember(Order = 4)]
-        public List<GithubTask> Steps { get; set; }
+        public new List<GithubTask> Steps { get; private set; }
+
+        [DefaultValue(0)]
+        [YamlMember(Order = 5, Alias = "timeout-minutes", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new int TimeoutInMinutes { get; private set; }
+
+        [YamlMember(Order = 6, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new Strategy Strategy { get; private set; }
+
+        [YamlMember(Order = 7, Alias = "env", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new Dictionary<string, string> EnvironmentVariables { get; private set; }
+
+        [YamlMember(Order = 8, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new Dictionary<string, string> Outputs { get; private set; }
+
+        [YamlMember(Order = 9, Alias = "needs", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new string[] Needs { get; private set; }
+
+        [YamlMember(Order = 10, Alias = "if", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new string If { get; private set; }
     }
 }
 
