d694b1076461afa3aac4fc004bd45bb1ee74e1f5
Christo du Toit <christo@dutoit.co.uk>
2023-06-02 19:24:59 +0100

    INFRASTRUCTURE: Created ConfigureGitTask

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/ConfigureGitTask.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
+{
+    /// <summary>
+    /// A task to build .NET project.
+    /// </summary>
+    public sealed class ConfigureGitTask : GithubTask
+    {
+        public ConfigureGitTask(string displayName = "GitHub Action", string email = "action@github.com")
+        {
+            this.Run = $"git config user.name \"{displayName}\"\r"
+                + $"git config user.email \"{email}\"";
+        }
+
+        /// <summary>
+        /// Gets the command to execute for the task.
+        /// </summary>
+        [YamlMember(Order = 7, Alias = "run")]
+        public new string Run { get; private set; }
+    }
+}
