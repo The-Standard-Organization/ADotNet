69588c5d1aafef3ca0e9ebf0ad0266e4aa66d38b
Christo du Toit <christo@dutoit.co.uk>
2023-06-02 19:12:15 +0100

    INFRASTRUCTURE: Defined ExtractProjectPropertyTask

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/ExtractProjectPropertyTask.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
+{
+    /// <summary>
+    /// A task to build .NET project.
+    /// </summary>
+    public sealed class ExtractProjectPropertyTask : GithubTask
+    {
+        public ExtractProjectPropertyTask(
+            string projectRelativePath,
+            string propertyName,
+            string environmentVariableName)
+        {
+            Run = environmentVariableName + "=$(awk -v RS='' -F'</?" + propertyName + ">' 'NF>1{print $2}' "
+                + $"{projectRelativePath} | sed -e 's/^[[:space:]]*//')\n"
+                + "echo '" + environmentVariableName + "<<EOF' >> $GITHUB_ENV\n"
+                + "echo -e \"$" + environmentVariableName + "\" >> $GITHUB_ENV\n"
+                + "echo 'EOF' >> $GITHUB_ENV\n"
+                + "echo \"" + propertyName + " - ${{ env." + environmentVariableName + " }}\"";
+        }
+
+        /// <summary>
+        /// Gets the command to execute for the task.
+        /// </summary>
+        [YamlMember(Order = 7, Alias = "run")]
+        public new string Run { get; private set; }
+    }
+}
