d2d2e785d8c08c07eeef892299c40592dac5245b
Christo du Toit <christo@dutoit.co.uk>
2023-06-02 19:13:19 +0100

    CODE RUB:  Upgraded GithubTask to support all the components as per GitHub spec

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/GithubTask.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/GithubTask.cs
@@ -4,13 +4,40 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.ComponentModel;
 using YamlDotNet.Serialization;
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
 {
     public class GithubTask
     {
-        [YamlMember(Order = 0)]
-        public string Name { get; set; }
+        [YamlMember(Order = 0, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual string Name { get; set; }
+
+        [YamlMember(Order = 1, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual string Id { get; set; }
+
+        [YamlMember(Order = 2, Alias = "needs", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual string[] Needs { get; set; }
+
+        [YamlMember(Order = 3, Alias = "if", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual string If { get; set; }
+
+        [YamlMember(Order = 4, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual string Uses { get; set; }
+
+        [YamlMember(Order = 5, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual Dictionary<string, string> With { get; set; }
+
+        [YamlMember(Order = 6, Alias = "env", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual Dictionary<string, string> EnvironmentVariables { get; set; }
+
+        [YamlMember(Order = 7, Alias = "run", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual string Run { get; set; }
+
+        [DefaultValue(0)]
+        [YamlMember(Order = 8, Alias = "timeout-minutes", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public virtual int TimeoutInMinutes { get; set; }
     }
 }
