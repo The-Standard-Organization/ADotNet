3217e27136dc07e044a6763d36b0573c80150fa8
Mabrouk Mahdhi <coding@mahdhi.com>
2025-01-24 01:56:10 +0100

    DATA: InstallPlaywrightBrowsersTask

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/InstallPlaywrightBrowsersTask.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
+{
+    public class InstallPlaywrightBrowsersTask : GithubTask
+    {
+        [YamlIgnore]
+        public string ProjectName { get; set; }
+        public override string Run => $"pwsh ./{ProjectName}/bin/Debug/net9.0/playwright.ps1 install";
+    }
+}
