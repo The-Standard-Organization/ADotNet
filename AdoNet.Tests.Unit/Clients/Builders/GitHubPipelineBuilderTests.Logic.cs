2f4f6eddf0114ca00de7675ef8f30faeaeca1639
Mabrouk Mahdhi <coding@mahdhi.com>
2025-01-24 15:32:26 +0100

    ShouldCreateNewPipeline -> PASS

---
+++ AdoNet.Tests.Unit/Clients/Builders/GitHubPipelineBuilderTests.Logic.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using ADotNet.Clients.Builders;
+using FluentAssertions;
+using Xunit;
+
+namespace ADotNet.Tests.Unit.Clients.Builders
+{
+    public partial class GitHubPipelineBuilderTests
+    {
+        [Fact]
+        public void ShouldCreateNewPipeline()
+        {
+            // given..when
+            var builder = GitHubPipelineBuilder.CreateNewPipeline();
+
+            // then
+            builder.Should().NotBeNull();
+        }
+    }
+}
---
+++ AdoNet.Tests.Unit/Clients/Builders/GitHubPipelineBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using ADotNet.Clients.Builders;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+
+namespace ADotNet.Tests.Unit.Clients.Builders
+{
+    public partial class GitHubPipelineBuilderTests
+    {
+        private static GithubPipeline GetPipeline(GitHubPipelineBuilder builder)
+        {
+            var privateField = typeof(GitHubPipelineBuilder)
+                .GetField("githubPipeline", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            return (GithubPipeline)privateField.GetValue(builder);
+        }
+    }
+}
