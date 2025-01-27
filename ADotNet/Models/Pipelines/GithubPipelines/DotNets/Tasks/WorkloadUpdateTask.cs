b248ad09fa9c96ea6768f09a6170303c46a98bb4
Mabrouk Mahdhi <16063715+mabroukmahdhi@users.noreply.github.com>
2025-01-27 18:11:21 +0100

    CODE RUB: Add Summary
    Co-authored-by: Christo du Toit <christo@dutoit.co.uk>

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/WorkloadUpdateTask.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/WorkloadUpdateTask.cs
@@ -6,8 +6,22 @@
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
 {
+    /// <summary>
+    /// Represents a task that updates the installed workloads in the .NET SDK.
+    /// </summary>
     public class WorkloadUpdateTask : GithubTask
     {
+        /// <summary>
+        /// Gets or sets the name of the task.
+        /// The default value is: "Update installed workloads in the .NET SDK".
+        /// </summary>
+        public override string Name { get; set; } = "Update installed workloads in the .NET SDK";
+
+        /// <summary>
+        /// Gets or sets the command to execute for the task.
+        /// The command updates all installed workloads in the .NET SDK to their latest versions.
+        /// The default value is: "dotnet workload update".
+        /// </summary>
         public override string Run { get; set; } = "dotnet workload update";
     }
 }
