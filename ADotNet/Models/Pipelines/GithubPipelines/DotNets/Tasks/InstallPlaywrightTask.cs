3efb68194308ff270cb678beb17183cf976d4d83
Mabrouk Mahdhi <16063715+mabroukmahdhi@users.noreply.github.com>
2025-01-27 18:08:12 +0100

    CODE RUB: Add Summary
    Co-authored-by: Christo du Toit <christo@dutoit.co.uk>

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/InstallPlaywrightTask.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/InstallPlaywrightTask.cs
@@ -6,8 +6,20 @@
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks
 {
+    /// <summary>
+    /// A task to install Microsoft.Playwright.CLI.
+    /// </summary>
     public class InstallPlaywrightTask : GithubTask
     {
+        /// <summary>  
+        /// Gets or sets the name of task.  
+        /// </summary>  
+        public override string Name { get; set; } = "Install Microsoft.Playwright.CLI.";  
+
+        /// <summary>
+        /// Gets or sets the command to execute for the task.
+        /// The default value is: "dotnet tool install --global Microsoft.Playwright.CLI".
+        /// </summary>
         public override string Run { get; set; } = "dotnet tool install --global Microsoft.Playwright.CLI";
     }
 }
