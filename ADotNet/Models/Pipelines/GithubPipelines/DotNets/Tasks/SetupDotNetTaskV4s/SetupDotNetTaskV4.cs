bbbfc97f4b6ece9a9ee88f66b65014d81cd4505c
Christo du Toit <christo@dutoit.co.uk>
2026-05-13 11:43:48 +0100

    CODE RUB: Updated Setup, Target, Publish And Tag Component

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/SetupDotNetTaskV4s/SetupDotNetTaskV4.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV4s
+{
+    public class SetupDotNetTaskV4 : GithubTask
+    {
+        public SetupDotNetTaskV4()
+        {
+            this.Uses = "actions/setup-dotnet@v4";
+        }
+
+        /// <summary>
+        /// Represents the usage of an external action or a specific version of an action in a GitHub Actions job step.
+        /// Default value: actions/setup-dotnet@v4
+        /// </summary>
+        [YamlMember(Order = 4, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new string Uses { get; private set; }
+
+        /// <summary>
+        /// Used to provide additional configuration or parameters for a specific step in the workflow.
+        /// </summary>
+        [YamlMember(Order = 5, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public new TargetDotNetVersionV4 With { get; set; }
+    }
+}
---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Tasks/SetupDotNetTaskV4s/TargetDotNetVersionV4.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV4s
+{
+    public class TargetDotNetVersionV4
+    {
+        [YamlMember(Alias = "dotnet-version")]
+        public string DotNetVersion { get; set; }
+    }
+}
