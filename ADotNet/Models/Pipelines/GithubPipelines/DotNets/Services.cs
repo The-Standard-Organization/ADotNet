fd9b20e3f55719f88881f22e5d4919bb05633835
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-07 22:35:34 +0100

    DATA: Services

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Services.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    public class Service
+    {
+        [YamlMember(Order = 0, Alias = "image", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public string Image { get; set; }
+
+        [YamlMember(Order = 1, Alias = "env", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public Dictionary<string, string> Environment { get; set; }
+
+        [YamlMember(Order = 2, Alias = "ports", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public List<string> Ports { get; set; }
+
+        [YamlMember(Order = 3, Alias = "options", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public string Options { get; set; }
+    }
+}
