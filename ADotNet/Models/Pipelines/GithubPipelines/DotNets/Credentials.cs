b583bf04da921c21a5cfa0cdfc0c6c405f8dfd44
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-08 13:14:44 +0100

    DATA: Added Credentials model

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Credentials.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    public class Credentials
+    {
+        [YamlMember(Order = 0, Alias = "username", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public string Username { get; set; }
+
+        [YamlMember(Order = 1, Alias = "password", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public string Password { get; set; }
+    }
+}
