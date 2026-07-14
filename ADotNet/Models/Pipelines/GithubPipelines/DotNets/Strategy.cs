64fe840d713272fed7f4269ccc33cbedad13615c
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-14 14:28:04 +0100

    DATA: Added StrategyV2 and cleaned up Strategy

--- ADotNet/Models/Pipelines/GithubPipelines/DotNets/Strategy.cs
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/Strategy.cs
@@ -10,25 +10,11 @@ using YamlDotNet.Serialization;
 
 namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
 {
+    [Obsolete("No longer in use. Please migrate to StrategyV2.")]
     public class Strategy
     {
-        [YamlMember(Order = 0, Alias = "fail-fast", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public bool? FailFast { get; set; }
-
-        [YamlMember(Order = 1, Alias = "max-parallel", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public int? MaxParallel { get; set; }
-
         [Obsolete("This property is now obsolete. Please migrate to MatrixV2.")]
-        [YamlMember(Order = 2, Alias = "matrix", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        [YamlMember(Order = 0, Alias = "matrix", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
         public Dictionary<string, string> Matrix { get; set; }
-
-        [YamlMember(Order = 3, Alias = "matrix", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public Dictionary<string, object> MatrixV2 { get; set; }
-
-        [YamlMember(Order = 4, Alias = "include", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public List<Dictionary<string, string>> Include { get; set; }
-
-        [YamlMember(Order = 5, Alias = "exclude", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
-        public List<Dictionary<string, string>> Exclude { get; set; }
     }
 }
---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/StrategyV2.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    public class StrategyV2
+    {
+        [YamlMember(Order = 0, Alias = "fail-fast", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public bool? FailFast { get; set; }
+
+        [YamlMember(Order = 1, Alias = "max-parallel", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public int? MaxParallel { get; set; }
+
+        [YamlMember(Order = 3, Alias = "matrix", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public Dictionary<string, object> Matrix { get; set; }
+
+        [YamlMember(Order = 4, Alias = "include", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public List<Dictionary<string, string>> Include { get; set; }
+
+        [YamlMember(Order = 5, Alias = "exclude", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public List<Dictionary<string, string>> Exclude { get; set; }
+    }
+}
