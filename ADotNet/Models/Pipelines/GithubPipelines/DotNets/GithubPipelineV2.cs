d85f5a76725514015e9ac23f33ea1c2eceb6b783
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-14 14:29:06 +0100

    CODERUB: Added GithubPipelineV2

---
+++ ADotNet/Models/Pipelines/GithubPipelines/DotNets/GithubPipelineV2.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright (c) Hassan Habib & Shri Humrudha Jagathisun All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace ADotNet.Models.Pipelines.GithubPipelines.DotNets
+{
+    public class GithubPipelineV2
+    {
+        public string Name { get; set; }
+
+        [YamlMember(Alias = "on")]
+        public Events OnEvents { get; set; }
+
+        [YamlMember(Alias = "env", DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+        public Dictionary<string, string> EnvironmentVariables { get; set; }
+
+        [YamlMember(Alias = "jobs")]
+        public Dictionary<string, JobV2> Jobs { get; set; }
+    }
+}
